4a41e424612214fcd40a6ebc87dbdd77646e4bb5
undraa0309 <133347765+undraa0309@users.noreply.github.com>
2024-05-27 18:49:02 +0900

    Update model (3).py

--- model (3).py
+++ model (3).py
@@ -3,56 +3,52 @@
 
 # In[1]:
 
-
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
-class CharRNN(nn.Module):
-    def __init__(self, vocab_size, n_hidden=256, n_layers=2, drop_prob=0.5):
-        super(CharRNN, self).__init__()
-        self.n_hidden = n_hidden
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
         self.n_layers = n_layers
 
-        self.embedding = nn.Embedding(vocab_size, n_hidden)
-        self.rnn = nn.RNN(n_hidden, n_hidden, n_layers, dropout=drop_prob, batch_first=True)
-        self.dropout = nn.Dropout(drop_prob)
-        self.fc = nn.Linear(n_hidden, vocab_size)
+        self.encoder = nn.Embedding(input_size, hidden_size)
+        self.rnn = nn.RNN(hidden_size, hidden_size, n_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x, hidden):
-        x = self.embedding(x)
-        r_output, hidden = self.rnn(x, hidden)
-        out = self.dropout(r_output)
-        out = out.contiguous().view(-1, self.n_hidden)
-        out = self.fc(out)
-        return out, hidden
+    def forward(self, input, hidden):
+        input = self.encoder(input.view(1, -1))
+        output, hidden = self.rnn(input.view(1, 1, -1), hidden)
+        output = self.decoder(output.view(1, -1))
+        return output, hidden
 
-    def init_hidden(self, batch_size):
-        weight = next(self.parameters()).data
-        hidden = weight.new(self.n_layers, batch_size, self.n_hidden).zero_()
-        return hidden
+    def init_hidden(self):
+        return torch.zeros(self.n_layers, 1, self.hidden_size)
 
-class CharLSTM(nn.Module):
-    def __init__(self, vocab_size, n_hidden=256, n_layers=2, drop_prob=0.5):
-        super(CharLSTM, self).__init__()
-        self.n_hidden = n_hidden
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(LSTM, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
         self.n_layers = n_layers
 
-        self.embedding = nn.Embedding(vocab_size, n_hidden)
-        self.lstm = nn.LSTM(n_hidden, n_hidden, n_layers, dropout=drop_prob, batch_first=True)
-        self.dropout = nn.Dropout(drop_prob)
-        self.fc = nn.Linear(n_hidden, vocab_size)
+        self.encoder = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, n_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x, hidden):
-        x = self.embedding(x)
-        r_output, hidden = self.lstm(x, hidden)
-        out = self.dropout(r_output)
-        out = out.contiguous().view(-1, self.n_hidden)
-        out = self.fc(out)
-        return out, hidden
+    def forward(self, input, hidden):
+        input = self.encoder(input.view(1, -1))
+        output, hidden = self.lstm(input.view(1, 1, -1), hidden)
+        output = self.decoder(output.view(1, -1))
+        return output, hidden
 
-    def init_hidden(self, batch_size):
-        weight = next(self.parameters()).data
-        hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_(),
-                  weight.new(self.n_layers, batch_size, self.n_hidden).zero_())
-        return hidden
+    def init_hidden(self):
+        return (torch.zeros(self.n_layers, 1, self.hidden_size),
+                torch.zeros(self.n_layers, 1, self.hidden_size))
 
+
+        
